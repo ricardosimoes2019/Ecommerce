54452e5e6914f5f0f3c0a900214f5f5ca9265b70
ricardosimoes2019 <35188@ufp.edu.pt>
2020-10-07 15:27:53 +0100

    First Commit

---
+++ backend/data.js
@@ -0,0 +1,39 @@
+export default {
+    products: [
+        {
+            _id: '1',
+            name: 'iPhone 11 Pro Max',
+            category: 'Apple',
+            image: '/images/iphone11ProMax.png',
+            price: 1400,
+            brand: 'Apple',
+            rating: 4.5,
+            numReviews: 10,
+            description: "Teste 123",
+            countInStock: 0,
+        },
+        {
+            _id: '2',
+            name: 'iPhone 11 Pro',
+            category: 'Apple',
+            image: '/images/iphone11Pro.png',
+            price: 1200,
+            brand: 'Apple',
+            rating: 4.3,
+            numReviews: 15,
+            description: "Teste 123",
+            countInStock: 8,
+        },
+        {
+            _id: '3',
+            name: 'iPhone 11',
+            category: 'Apple',
+            image: '/images/iphone11.png',
+            price: 1000,
+            brand: 'Apple',
+            rating: 4.5,
+            numReviews: 12,
+            description: "Teste 123",
+            countInStock: 10,
+        }]
+}
